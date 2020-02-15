34874b29e109e94e134d77f37bebcf6414c4e144
Carlos K <carloskotachoamd@hotmail.com>
2020-02-15 19:01:26 -0300

    Adicionado input e button em pesquisar pessoas

--- src/app/persons/persons.module.ts
+++ src/app/persons/persons.module.ts
@@ -1,11 +1,19 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { PersonSearchComponent } from './person-search/person-search.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule
   ],
   declarations: [
     PersonSearchComponent

8f41fb395504980f79514b92499a27b723a5670f
Carlos K <carloskotachoamd@hotmail.com>
2020-02-15 23:04:29 -0300

    Adicionado component footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }

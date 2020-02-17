2fed04aab04d322cd075a74e380e929d88d1a08d
Carlos K <carloskotachoamd@hotmail.com>
2020-02-17 18:35:21 -0300

    Exclusão do método search

--- src/app/persons/person-search/person-search.component.ts
+++ src/app/persons/person-search/person-search.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { LazyLoadEvent } from 'primeng/components/common/api';
-
-import { PersonService, PersonFilter } from './../person.service';
-
 @Component({
   selector: 'app-person-search',
   templateUrl: './person-search.component.html',
@@ -11,29 +7,22 @@ import { PersonService, PersonFilter } from './../person.service';
 })
 export class PersonSearchComponent implements OnInit {
 
-  totalRecords = 0;
-  persons = [];
-  filter = new PersonFilter();
+  persons: any;
 
-  constructor(
-    private personService: PersonService
-  ) {}
+  constructor() {}
 
-  ngOnInit() {}
-
-  search(page = 0) {
-    this.filter.page = page;
-
-    this.personService.search(this.filter)
-      .then(result => {
-        this.totalRecords = result.count;
-        this.persons = result.persons;
-      });
+  ngOnInit() {
+    this.persons = [
+      { name: 'Luke Skywalker', height: '172', mass: '77', hair_color: 'blond', skin_color: 'fair',
+       eye_color: 'blue', birth_year: '19BBY', gender: 'male', homeworld: 'Tatooine' },
+      { name: 'C-3PO', height: '167', mass: '75', hair_color: 'n/a', skin_color: 'gold',
+       eye_color: 'yellow', birth_year: '112BBY', gender: 'n/a', homeworld: 'Tatooine' },
+      { name: 'R2-D2', height: '96', mass: '32', hair_color: 'n/a', skin_color: 'white, blue',
+       eye_color: 'red', birth_year: '33BBY', gender: 'n/a', homeworld: 'Naboo' },
+      { name: 'Darth Vader', height: '202', mass: '136', hair_color: 'none', skin_color: 'white',
+       eye_color: 'yellow', birth_year: '41.9BBY', gender: 'male', homeworld: 'Tatooine' },
+      { name: 'Leia Organa', height: '150', mass: '49', hair_color: 'brown', skin_color: 'light',
+       eye_color: 'brown', birth_year: '19BBY', gender: 'female', homeworld: 'Alderaan' }
+    ];
   }
-
-  whenChangingPages(event: LazyLoadEvent) {
-    const page = event.first / event.rows;
-    this.search(page);
-  }
-
 }
--- src/app/persons/person.service.ts
+++ src/app/persons/person.service.ts
@@ -1,51 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, URLSearchParams, Headers } from '@angular/http';
-
-import 'rxjs/add/operator/toPromise';
-
-import { environment } from './../../environments/environment.prod';
-
-export class PersonFilter {
-  search: string;
-  page = 0;
-}
 
 @Injectable()
 export class PersonService {
 
-  personsUrl: string;
-
-  constructor(private http: Http) {
-    this.personsUrl = `${environment.apiUrl}/people`;
-  }
-
-  search(filter: PersonFilter): Promise<any> {
-
-    // const headers = new Headers();
-    // headers.append('Content-Type', 'application/json');
-
-    const params = new URLSearchParams();
-
-    params.set('page', filter.page.toString());
-
-    if (filter.search) {
-      params.set('search', filter.search);
-    }
-
-    return this.http.get(`${this.personsUrl}`, { search: params })
-      .toPromise()
-      .then(response => {
-        const responseJson = response.json();
-        const persons = responseJson.results;
-
-        const result = {
-          persons,
-          count: responseJson.count
-        };
-
-        return result;
-      });
-
-  }
-
+  constructor() {}
 }

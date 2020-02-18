f8c2080279d168e86659b3f27fc088bd2fc727fd
Carlos K <carloskotachoamd@hotmail.com>
2020-02-17 23:28:47 -0300

    Refatorando

--- src/app/persons/person-search/person-search.component.ts
+++ src/app/persons/person-search/person-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { PersonService } from './../person.service';
+import { PersonService } from '../person.service';
 
 @Component({
   selector: 'app-person-search',

1592f947434639c157873e7e1da0136ab9feb881
Carlos K <carloskotachoamd@hotmail.com>
2020-05-02 15:09:56 -0300

    Alterado: swapi.co para swapi.dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://swapi.co/api'
+  apiUrl: 'https://swapi.dev/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://swapi.co/api'
+  apiUrl: 'https://swapi.dev/api'
 };
